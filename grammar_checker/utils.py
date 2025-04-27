9b944fd20fdff2967bd9dc116372ffe96fe56a5a
gititinyoursoul <marius.hartmann@hotmail.com>
2025-04-27 21:46:46 +0200

    minor changes to log string formating

--- grammar_checker/utils.py
+++ grammar_checker/utils.py
@@ -7,22 +7,22 @@ logger = get_logger(__name__)
 def load_test_cases(file_path):
     try:
         with open(file_path, "r", encoding="utf-8") as file:
-            logger.info(f"Loading test cases from {file_path}")
+            logger.info(f"Loading test cases from '{file_path}'")
             return json.load(file)
     except FileNotFoundError as e:
-        logger.error(f"Test cases file not found: {file_path}: {e}")
-        raise FileNotFoundError(f"Test cases file not found: {file_path}") from e
+        logger.error(f"Test cases file not found: '{file_path}': {e}")
+        raise FileNotFoundError(f"Test cases file not found: '{file_path}'") from e
     except json.JSONDecodeError as e:
-        logger.error(f"Error decoding JSON from file {file_path}: {e}")
-        raise ValueError(f"Invalid JSON format in file {file_path}") from e
+        logger.error(f"Error decoding JSON from file '{file_path}': {e}")
+        raise ValueError(f"Invalid JSON format in file '{file_path}'") from e
 
 # save test results to a json file
 def save_test_results(file_path, results):
     try:
         with open(file_path, "w", encoding="utf-8") as file:
             json.dump(results, file, indent=4)  
-        logger.info(f"Test results saved to {file_path}")
+        logger.info(f"Test results saved to '{file_path}'")
     except Exception as e:
-        logger.error(f"Error saving test results to file {file_path}: {e}")
+        logger.error(f"Error saving test results to file '{file_path}': {e}")
         raise 
     
