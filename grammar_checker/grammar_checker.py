d403eb4f5eee22ba9833c6144508be128c677fd5
gititinyoursoul <marius.hartmann@hotmail.com>
2025-04-28 00:59:32 +0200

    Removed self.prompt_template from class, now accessed through PromptBuilder object.

--- grammar_checker/grammar_checker.py
+++ grammar_checker/grammar_checker.py
@@ -1,24 +1,26 @@
 from grammar_checker.logger import get_logger
 from grammar_checker.prompt_builder import PromptBuilder
 from grammar_checker.openai_client import OpenAIClient
-from grammar_checker.evaluator import evaluate_result
+from grammar_checker.evaluator import evaluate_response
 
 logger = get_logger(__name__)
 
+
 class GrammarChecker:
-    def __init__(self, prompt_template :str, sentence: str, model: str, prompt_builder: PromptBuilder, client: OpenAIClient):
-        self.prompt_template = prompt_template
+    def __init__(self, prompt_builder: PromptBuilder, sentence: str, model: str, client: OpenAIClient):
+        self.prompt_builder = prompt_builder
         self.sentence = sentence
         self.model = model
-        self.prompt_builder = prompt_builder
         self.client = client
-        
+
         logger.info(f"GrammarChecker initialized with model: {model}, sentence: {sentence}")
 
     def check_grammar(self):
         prompt = self.prompt_builder.build_prompt(self.sentence)
         try:
-            response = self.client.get_model_response(self.model, prompt, self.prompt_template, self.sentence)
+            response = self.client.get_model_response(
+                self.model, prompt, self.prompt_builder.template_path, self.sentence
+            )
             if response:
                 return response
             else:
