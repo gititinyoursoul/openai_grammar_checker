b9447dbd7b63d1a8ca360697ad8d2700e1189ee9
gititinyoursoul <marius.hartmann@hotmail.com>
2025-05-19 21:26:32 +0200

    feat: add FastAPI server mode and enhance argument parsing in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from dotenv import load_dotenv
+import uvicorn
 from runner import main as run_tests
 from interactive import main as start_interactive_mode
 from grammar_checker.logger import get_logger
@@ -19,9 +20,9 @@ def parse_arguments():
 
     parser.add_argument(
         "--mode",
-        choices=["interactive", "run_tests"],
+        choices=["interactive", "api", "run_tests"],
         default="interactive",
-        help="Choose mode: 'interactive' for user input, 'run_tests' for test cases",
+        help="Choose mode: 'interactive' for user input, 'api' to start the FastAPI server or 'run_tests' to run test cases",
     )
 
     parser.add_argument(
@@ -31,6 +32,16 @@ def parse_arguments():
         help="Specify where to save the test results: 'save_to_db' for database or 'save_to_file' for a JSON file (default: 'save_to_db').",
     )
 
+    # arguments for FastAPI server
+    parser.add_argument(
+        "--api-key",
+        default=os.getenv("OPENAI_API_KEY"),
+        help="OpenAI API key (default: from environment variable).",
+    )
+    parser.add_argument("--api-host", default="127.0.0.1")
+
+    parser.add_argument("--api-port", type=int, default=8000)
+
     parser.add_argument(
         "--models",
         nargs="+",
@@ -39,15 +50,15 @@ def parse_arguments():
     )
 
     parser.add_argument(
-        "--test-cases",
-        default=TEST_CASES_FILE_REF,
-        help="Path to the test cases JSON file (default: from config).",
-    )
-
-    parser.add_argument(
         "--prompt-template",
         default=PROMPT_TEMPLATE,
         help="Path to the prompt template file (default: from config).",
+    )
+
+    parser.add_argument(
+        "--test-cases",
+        default=TEST_CASES_FILE_REF,
+        help="Path to the test cases JSON file (default: from config).",
     )
 
     parser.add_argument(
@@ -63,7 +74,7 @@ def parse_arguments():
 def main():
     logger = get_logger(__name__)
     logger.info("Logging configuration set up successfully.")
-    
+
     load_dotenv()  # Load environment variables from .env file
     logger.info("Environment variables loaded successfully.")
 
@@ -74,7 +85,6 @@ def main():
     logger.info("MongoDB handler initialized successfully.")
 
     if args.mode == "run_tests":
-        logger.info("run_tests executed successfully.")
         run_tests(
             test_cases_file=args.test_cases,
             models=args.models,
@@ -82,10 +92,16 @@ def main():
             prompt_template=args.prompt_template,
             db_handler=db_handler,
         )
-
-    else:
-        logger.info("start_interactive_mode started successfully.")
+        logger.info("run_tests executed successfully.")
+    elif args.mode == "api":
+        uvicorn.run("api:app", host=args.api_host, port=args.api_port, reload=True)
+        logger.info("API mode started successfully.")
+    elif args.mode == "interactive":
         start_interactive_mode(db_handler)
+        logger.info("Interactive mode started successfully.")
+    else:
+        logger.error("Invalid mode selected. Please choose 'interactive', 'api', or 'run_tests'.")
+        raise ValueError("Invalid mode selected. Please choose 'interactive', 'api', or 'run_tests'.")
 
 
 if __name__ == "__main__":
