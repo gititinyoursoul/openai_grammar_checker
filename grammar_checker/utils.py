62f489c89c6ad5d4a988ced06390d0fbda88fb7d
gititinyoursoul <marius.hartmann@hotmail.com>
2025-06-12 17:22:42 +0200

    fix: update save_test_results function to correctly format results before saving

--- grammar_checker/utils.py
+++ grammar_checker/utils.py
@@ -1,5 +1,5 @@
 import json
-from grammar_checker.logger import get_logger
+from grammar_checker.logger import get_logger, log_path
 
 logger = get_logger(__name__)
 
@@ -20,9 +20,17 @@ def load_test_cases(file_path):
 
 # save test results to a json file
 def save_test_results(file_path, results):
+    results_dump = []
+
+    for result in results:
+        request = result["request"].model_dump()
+        response = result["response"].model_dump()
+        benchmark_eval = result["benchmark_eval"]
+        results_dump.append({"request": request, "response": response, "benchmark_eval": benchmark_eval})
+
     try:
         with open(file_path, "w", encoding="utf-8") as file:
-            json.dump(results, file, indent=4)
+            json.dump(results_dump, file, indent=4)
         logger.info(f"Test results saved to '{file_path}'")
     except Exception as e:
         logger.error(f"Error saving test results to file '{file_path}': {e}")
