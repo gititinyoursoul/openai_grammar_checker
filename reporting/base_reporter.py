b95a4e51c7b9b51360569f7f41e4074f75471033
gititinyoursoul <marius.hartmann@hotmail.com>
2025-06-16 00:12:34 +0200

    refactor: streamline file path generation in BenchmarkReporter and update CSVReporter to use inherited method

--- reporting/base_reporter.py
+++ reporting/base_reporter.py
@@ -1,7 +1,23 @@
 from abc import ABC, abstractmethod
 from typing import Any
+from pathlib import Path
+from datetime import datetime as dt
+from grammar_checker.config import REPORTS_DIR
+
 
 class BenchmarkReporter(ABC):
+    extension = "txt"
+
+    def __init__(self, output_dir: Path = REPORTS_DIR):
+        self.output_dir = Path(output_dir)
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+
+    def _make_file_path(self, file_name: str) -> Path:
+        """Generate a timestamped file path in the output directory."""
+        timestamp = dt.now().strftime("%Y%m%d%H%M%S")
+        full_name = f"{timestamp}_{file_name}.{self.extension}"
+        return self.output_dir / full_name
+
     @abstractmethod
     def report(self, file_name: str, data: Any):
         """Process and output benchmark results."""
--- reporting/csv_reporter.py
+++ reporting/csv_reporter.py
@@ -1,23 +1,24 @@
-from pathlib import Path
 import pandas as pd
-from .base_reporter import BenchmarkReporter
+from reporting.base_reporter import BenchmarkReporter
 from grammar_checker.logger import get_logger, log_path
-from grammar_checker.config import REPORTS_DIR
 
 
 logger = get_logger(__name__)
 
 
 class CSVReporter(BenchmarkReporter):
-    def __init__(self, output_dir: str = REPORTS_DIR):
-        self.output_dir = Path(output_dir)
-        self.output_dir.mkdir(parents=True, exist_ok=True)
+    extension = "csv"
 
-    def report(self, file_name: str, data):
-        file_path = self.output_dir / f"{file_name}.csv"
+    def __init__(self):
+        super().__init__()
+
+    def report(self, file_name: str, data: pd.DataFrame):
+        file_path = self._make_file_path(file_name)
 
         if isinstance(data, pd.DataFrame):
             data.to_csv(file_path, index=False)
-            logger.info(f"Report {file_name}.csv saved in '{log_path(REPORTS_DIR)}'.")
+            logger.info(f"Report {file_path.name} saved in '{log_path(file_path.parent)}'.")
         else:
-            raise TypeError(f"CSVReporter cannot handle data type: {type(data)}")
+            error_msg = f"CSVReporter cannot handle data type: {type(data)}"
+            logger.error(error_msg)
+            raise TypeError(error_msg)
