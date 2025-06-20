ca7d68e7fdfa5d7daaa1b343da63916ce2ade124
gititinyoursoul <marius.hartmann@hotmail.com>
2025-06-20 17:15:52 +0200

    refactor: update mistakes type in GrammarResponse to use dict instead of Mistake model

--- models/response.py
+++ models/response.py
@@ -2,22 +2,22 @@ from pydantic import BaseModel, Field
 from typing import List, Literal
 
 # Represents one individual mistake entry in the response
-class Mistake(BaseModel):
-    type: Literal[
-        "PunctuationMistake",
-        "WordOrderMistake",
-        "WrongArticleMistake",
-        "VerbTenseMistake",
-        "ComparativeFormMistake",
-        "SpellingMistake",
-        "CapitalizationMistake",
-        "OtherMistake"
-    ]
-    original: str
-    corrected: str
+# class Mistake(BaseModel):
+#     type: Literal[
+#         "PunctuationMistake",
+#         "WordOrderMistake",
+#         "WrongArticleMistake",
+#         "VerbTenseMistake",
+#         "ComparativeFormMistake",
+#         "SpellingMistake",
+#         "CapitalizationMistake",
+#         "OtherMistake"
+#     ]
+#     original: str
+#     corrected: str
     
 # Base schema used by API and interactive responses
 class GrammarResponse(BaseModel):
     input: str = Field(..., min_length=1)
-    mistakes: List[Mistake]
+    mistakes: List[dict]
     corrected_sentence: str =  Field(..., min_length=1)
